dcbf2d7bb0c93c60bc14eb90f0ea30a8dea535f8
Djordje Dimitrov <dd222im@student.lnu.se>
2024-04-25 08:19:16 +0200

    Fully functional edge impulse live inference for object detection.

--- main.cpp
+++ main.cpp
@@ -12,10 +12,7 @@ static float features[EI_CLASSIFIER_NN_INPUT_FRAME_SIZE];
 
 void extract_features_from_frame(const cv::Mat& frame) {
     cv::Mat processed;
-
-   
     cv::resize(frame, processed, cv::Size(EI_CLASSIFIER_INPUT_WIDTH, EI_CLASSIFIER_INPUT_HEIGHT));
-    
     cv::cvtColor(processed, processed, cv::COLOR_BGR2RGB);
 
     if (processed.isContinuous()) {
@@ -32,42 +29,44 @@ void extract_features_from_frame(const cv::Mat& frame) {
 
 // Callback function to provide data to the model
 static int get_signal_data(size_t offset, size_t length, float *out_ptr) {
-    if (offset + length > EI_CLASSIFIER_NN_INPUT_FRAME_SIZE) return -1; // Handle buffer overflow
+    if (offset + length > EI_CLASSIFIER_NN_INPUT_FRAME_SIZE) return -1; 
     for (size_t i = 0; i < length; ++i) {
         out_ptr[i] = static_cast<float>(features[offset + i]);
     }
     return EIDSP_OK;
 }
 
-void captureAndProcess() {
-    cv::VideoCapture cap(0); // open cam
+
+void runInference() {
+    cv::VideoCapture cap(0); 
     if (!cap.isOpened()) { 
         std::cerr << "ERROR: Could not open camera" << std::endl;
         return;
     }
 
-    cv::namedWindow("Live Detection", cv::WINDOW_AUTOSIZE);
-
-    ei_printf("Starting inferencing in 2 seconds...\r\n");
-    ei_printf("Please point the camera to the object you want to detect\r\n");
     int width = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_WIDTH));
     int height = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_HEIGHT));
+    /* FOR WRITING VIDEO
+    cv::VideoWriter video("output.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 25, cv::Size(width, height));
+    if (!video.isOpened()) {
+        std::cerr << "ERROR: Could not open video writer" << std::endl;
+        return;
+    }
+    */
 
-    ei_printf("Camera resolution: %d x %d\r\n", width, height);
+    cv::namedWindow("Live Detection", cv::WINDOW_AUTOSIZE);
+
     std::this_thread::sleep_for(std::chrono::seconds(2));
-
 
     cv::Mat frame;
     signal_t signal;
     ei_impulse_result_t result;
     EI_IMPULSE_ERROR res;
 
-
     while (true) {
         cap >> frame; 
         if (frame.empty()) break;
 
-        // Calculate scale factors
         float xScale = frame.cols / EI_CLASSIFIER_INPUT_WIDTH;
         float yScale = frame.rows / EI_CLASSIFIER_INPUT_HEIGHT;
 
@@ -75,12 +74,10 @@ void captureAndProcess() {
         signal.total_length = EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE;
         signal.get_data = &get_signal_data;
 
-        res = run_classifier(&signal, &result, false);  
-
-
+        res = run_classifier(&signal, &result, false);
         if (res != EI_IMPULSE_OK) {
             std::cerr << "ERROR: Failed to run classifier" << std::endl;
-            return;
+            break; 
         }
 
         for (uint32_t i = 0; i < EI_CLASSIFIER_OBJECT_DETECTION_COUNT; i++) {
@@ -89,48 +86,24 @@ void captureAndProcess() {
                 continue;
             }
 
-            ei_printf("count of bounding boxes: %d\r\n", result.bounding_boxes_count);
-
-            int x = bb.x + bb.width / 2;
-            int y = bb.y + bb.height / 2;
-
-            ei_printf("unscaled x: %d, y: %d\r\n", x, y);
-
-            // Scale the coordinates back to the original frame
-            x = x * xScale;
-            y = y * yScale;
-
-            ei_printf("scaled x: %d, y: %d\r\n", x, y);
-            
-            ei_printf("Object detected!:\r\n");
-            // print coordinates
-            ei_printf("  %s (%f) [ x: %u, y: %u, width: %u, height: %u ]\r\n",
-                    bb.label,
-                    bb.value,
-                    bb.x,
-                    bb.y,
-                    bb.width,
-                    bb.height);
-
-
+            int x = (bb.x + bb.width / 2) * xScale;
+            int y = (bb.y + bb.height / 2) * yScale;
             cv::circle(frame, cv::Point(x, y), 5, cv::Scalar(0, 255, 0), -1);
-            //ei_printf("Drawing circle at x: %d, y: %d\r\n", x, y);
-            //cv::putText(frame, bb.label, cv::Point(x, y), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 255, 0), 2);
         }
         
-
         cv::imshow("Live Detection", frame);
+        //video.write(frame); 
 
-        if (cv::waitKey(1) == 27) break; // Break the loop on pressing 'ESC' 
-
+        if (cv::waitKey(1) == 27) break; // ESC key
     }
 
-    cap.release(); // When everything done, release the video capture object
-    cv::destroyAllWindows(); // Close all OpenCV windows
+    cap.release(); 
+    //video.release();
+    cv::destroyAllWindows();
 }
 
 
 int main() {
-    captureAndProcess();
+    runInference();
     return 0;
 }
